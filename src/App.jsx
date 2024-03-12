469fac2620286eb15bd2b219c2b60476dc99be46
Ileolami <99335756+Ileolami@users.noreply.github.com>
2024-03-12 19:18:12 +0100

    Add axios and qs dependencies, and implement search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import './App.css'
 import SearchBar from './Features/Searchbar'
 import MusicPlayer from './Features/Musicplay'
 import DarkMode from './Features/Darkmode'
+import Login from './Features/Login'
+import { BrowserRouter,Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <DarkMode/>
-      <div className='flex flex-col my- mx-10 lg:m-8 gap-7 lg:flex-row lg:gap-12 '>
-        <SearchBar />
-        <MusicPlayer />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={
+          <div>
+            <DarkMode />
+            <div className='flex flex-col my- mx-10 lg:m-8 gap-7 lg:flex-row lg:gap-12 '>
+              <SearchBar />
+              <MusicPlayer />
+            </div>
+          </div>
+        } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
